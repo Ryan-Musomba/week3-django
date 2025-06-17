c2546d9ef4b79fec495098f387f3b9b2b753d098
Ryan-Musomba <ryanmusomba@gmail.com>
2025-06-17 10:41:31 +0300

    l

--- week3/settings.py
+++ week3/settings.py
@@ -64,12 +64,11 @@ TEMPLATES = [
         },
     },
 ]
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'socialmediaapp/static')]
